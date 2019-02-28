85eafe4c5c9ba3cf877f1fec5abfaabd827fd311
yael-sarusi <yael.sarusi@mail.huji.ac.il>
2019-02-28 20:54:39 +0200

    minor change in tags

--- Parser.py
+++ Parser.py
@@ -7,31 +7,35 @@ from Photo import Photo
 class Parser:
     @staticmethod
     def parse_file(file_name):
-        pictures = []
+        photos = []
         photo_index = 0
         tag_dictionary = dict()
         with open(file_name, 'r') as file:
-            pic_num = file.readline()
+            photo_num = file.readline()
             for line in file:
                 picture_data = line.split()
                 is_vertical = True if picture_data[0] == 'V' else False
                 photo_tags = picture_data[2:]
-                cur_picture = Photo(photo_index, photo_tags, is_vertical)
-                pictures.append(cur_picture)
+                cur_photo = Photo(photo_index, photo_tags, is_vertical)
+                photos.append(cur_photo)
                 photo_index += 1
                 for tag in photo_tags:
                     if tag in tag_dictionary:
-                        tag_dictionary[tag].append(cur_picture)
+                        tag_dictionary[tag].append(cur_photo)
                     else:
-                        tag_dictionary[tag] = [cur_picture]
+                        tag_dictionary[tag] = [cur_photo]
 
                 if is_vertical:
-                    if 'V' in tag_dictionary:
-                        tag_dictionary['V'].append(cur_picture)
-                    else:
-                        tag_dictionary['V'] = [cur_picture]
+	                photo_tags = photo_tags[:]
+	                photo_tags.append('V')
 
-        return pictures, tag_dictionary
+                for tag in photo_tags:
+	                if tag in tag_dictionary:
+		                tag_dictionary[tag].append(cur_photo)
+	                else:
+		                tag_dictionary[tag] = [cur_photo]
+
+        return cur_photo, tag_dictionary
 
 
 if __name__ == '__main__':
