0282c9b22ec554821b27ffe830a6f972d77de1d8
daphnaleventer <daphna.leventer@huji.mail.ac.il>
2019-02-28 20:09:17 +0200

    photo object added score function

--- Photo.py
+++ Photo.py
@@ -1,7 +1,16 @@
 class Photo:
-    def __init__(self, index, tags):
-        self.tags = tags
+    def __init__(self, index, tags, v_or_h):
+        self.tags = tags # a set
         self.image_index = index
+        self.v_or_h = v_or_h
+        self.is_taken = False
 
     def __contains__(self, a):
         return a in self.tags
+
+    def score(self, other):
+        common_tags = self.tags & other.tags
+        self_tags = self.tags - common_tags
+        other_tags = other.tags - common_tags
+        return min(common_tags, self_tags, other_tags)
+
